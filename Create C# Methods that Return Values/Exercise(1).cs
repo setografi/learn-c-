17733f792b5e8b619650639000b803426bc0ef4b
setografi <hitonaashbell@gmail.com>
2024-09-01 01:01:49 +0700

    Training 4

---
+++ Create C# Methods that Return Values/Exercise(1).cs
@@ -0,0 +1,21 @@
+//Return numbers from methods
+
+using System;
+
+double usd = 23.73;
+int vnd = UsdToVnd(usd);
+
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
+
+int UsdToVnd(double usd) 
+{
+    int rate = 23500;
+    return (int) (rate * usd);
+}
+
+double VndToUsd(int vnd) 
+{
+    double rate = 23500;
+    return vnd / rate;
+}
---
+++ Write Your First C# Method/Exercise(1).cs
@@ -0,0 +1,16 @@
+using System;
+
+Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
+
+void DisplayRandomNumbers() 
+{
+    Random random = new Random();
+
+    for (int i = 0; i < 5; i++) 
+    {
+        Console.Write($"{random.Next(1, 100)} ");
+    }
+
+    Console.WriteLine();
+}
